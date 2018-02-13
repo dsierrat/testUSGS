f1fe382e95e2014fc584ad230be12bce013e53ab
dsierra <dsierra@seidor.com>
2018-02-13 15:38:02 -0500

    Commit inicial, obteniendo data de USGS

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Threading;
+using System.Timers;
+using System.Windows.Forms;
+using TestUsgs;
+
+namespace ServiceVerifier
+{
+    public class Program
+    {
+        private static System.Timers.Timer aTimer;
+        public static bool firstTimeFails = false;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("The program have started");
+                // MessageBox.Show("The program have started successfully", "Program", MessageBoxButtons.OK);
+                aTimer = new System.Timers.Timer(10000);
+                aTimer.Elapsed += OnTimedEvent;
+                aTimer.AutoReset = true;
+                aTimer.Enabled = true;
+                new ManualResetEvent(false).WaitOne();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+        }
+
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            var usgsString = new WebClient().DownloadString("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2018-02-11&endtime=2018-02-12&minmagnitude=5");
+            var model = JsonConvert.DeserializeObject<USGSModel>(usgsString);
+            
+
+            foreach (USGSModel.Evento evento in model.Feature)
+            {
+                DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+                dtDateTime = dtDateTime.AddMilliseconds(evento.Propiedades.Hora).ToUniversalTime();
+                Console.WriteLine(evento.Propiedades.Titulo + " at " + dtDateTime);
+            }
+
+        }
+    }
+}
---
+++ USGSModel.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace TestUsgs
+{
+    public class USGSModel
+    {
+        [JsonProperty(PropertyName = "features")]
+        public List<Evento> Feature { get; set; }
+
+        public class Evento
+        {
+            [JsonProperty(PropertyName = "id")]
+            public string Id { get; set; }
+            [JsonProperty(PropertyName = "properties")]
+            public Propiedades Propiedades { get; set; }
+            [JsonProperty(PropertyName = "geometry")]
+            public Geometry Geometry { get; set; }
+        }
+
+        public class Propiedades
+        {
+            [JsonProperty(PropertyName = "mag")]
+            public double Magnitud { get; set; }
+            [JsonProperty(PropertyName = "place")]
+            public string Lugar { get; set; }
+            [JsonProperty(PropertyName = "time")]
+            public long Hora { get; set; }
+            [JsonProperty(PropertyName = "updated")]
+            public long Actualizado { get; set; }
+            [JsonProperty(PropertyName = "tz")]
+            public int tz { get; set; }
+            [JsonProperty(PropertyName = "url")]
+            public string Url { get; set; }
+            [JsonProperty(PropertyName = "detail")]
+            public string DetalleLink { get; set; }
+            //DYFI = Did you feel it 
+            [JsonProperty(PropertyName = "felt", NullValueHandling =NullValueHandling.Ignore)]
+            public int DYFINumber { get; set; }
+
+            //Mercalli intensity reported by DYFI
+            [JsonProperty(PropertyName = "cdi", NullValueHandling = NullValueHandling.Ignore)]
+            public double MercalliDYFI { get; set; }
+            //Mercalli intensity reported by shakeMap
+            [JsonProperty(PropertyName = "mmi", NullValueHandling = NullValueHandling.Ignore)]
+            public double MercalliMagnitud { get; set; }
+
+            [JsonProperty(PropertyName = "alert")]
+            public string Alerta { get; set; }
+            //“automatic”, “reviewed”, “deleted”
+            [JsonProperty(PropertyName = "status")]
+            public string Status { get; set; }
+            [JsonProperty(PropertyName = "tsunami")]
+            public int Tsunami { get; set; }
+            [JsonProperty(PropertyName = "sig", NullValueHandling = NullValueHandling.Ignore)]
+            public int Importancia { get; set; }
+            [JsonProperty(PropertyName = "code")]
+            public string Code { get; set; }
+            [JsonProperty(PropertyName = "magType")]
+            public string magType { get; set; }
+            [JsonProperty(PropertyName = "type")]
+            public string Tipo { get; set; }
+            [JsonProperty(PropertyName = "title")]
+            public string Titulo { get; set; }
+        }
+
+        public class Geometry
+        {
+            [JsonProperty(PropertyName = "type")]
+            public string Tipo_geo { get; set; }
+            [JsonProperty(PropertyName = "coordinates")]
+            public List<double> Cordenadas_geo { get; set; }
+        }
+
+    }
+}

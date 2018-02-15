6a9bf0f4c0c42ad25b875e61790bf06d52fbe95e
dsierra <dsierra@seidor.com>
2018-02-15 16:04:20 -0500

    Added email class, added beep

---
+++ Email.cs
@@ -0,0 +1,71 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net;
+//using System.Net.Mail;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace TestUsgs
+//{
+//    public class Email
+//    {
+//        public static void SendEmail(bool isRestarted)
+//        {
+//            try
+//            {
+//                SmtpClient client = new SmtpClient
+//                {
+//                    Port = 587,
+//                    EnableSsl = true,
+//                    Credentials = new NetworkCredential(config.EmailFrom, config.EmailPassword),
+//                    Host = config.EmailHost
+//                };
+
+//                //e-mail sender
+//                MailAddress from = new MailAddress(config.EmailFrom, config.EmailName, System.Text.Encoding.UTF8);
+//                //destinations for the e-mail message.
+//                MailAddress to = new MailAddress(config.EmailTo);
+
+//                //message content
+//                MailMessage message = new MailMessage(from, to)
+//                {
+//                    Body = "The service " + config.ServiceName + " has stopped",
+//                    BodyEncoding = System.Text.Encoding.UTF8,
+//                    Subject = config.ServiceName + " Service down",
+//                    SubjectEncoding = System.Text.Encoding.UTF8
+//                };
+//                message.Body += isRestarted ? ", but it was recovered by the recovery program.\n\n" : " and it couldn't be recovered please restart it.\n\n";
+//                message.Body += GetLastServiceError();
+
+//                // Set the method that is called back when the send operation ends.
+//                client.SendCompleted += new
+//                SendCompletedEventHandler(SendCompletedCallback);
+
+//                // The userState can be any object that allows your callback 
+//                string userState = "test message1";
+//                client.SendAsync(message, userState);
+//                log.LogServiceWrite("The Email was sended to " + config.EmailTo, "Info");
+//            }
+//            catch (Exception e)
+//            {
+//                log.LogServiceWrite("Error sending e-mail " + e.Message + "\n Source: " + e.Source, "Error");
+//            }
+//        }
+
+//        public static void SendCompletedCallback(object sender, AsyncCompletedEventArgs e)
+//        {
+//            //Get the unique identifier for this asynchronous operation.
+
+//            if (e.Error != null)
+//            {
+//                log.LogServiceWrite(("Error sending e-mail " + e.Error.ToString()), "Error");
+//            }
+//            else
+//            {
+//                log.LogServiceWrite("Message sent", "Info");
+//            }
+//        }
+
+//    }
+//}
--- Program.cs
+++ Program.cs
@@ -10,9 +10,7 @@ using TestUsgs;
 namespace ServiceVerifier
 {
     public class Program
-    {
-        private static System.Timers.Timer aTimer;
-        public static bool firstTimeFails = false;
+    { 
         public static MySqlConnection con;
 
         static void Main(string[] args)
@@ -20,79 +18,109 @@ namespace ServiceVerifier
             try
             {
                 Console.WriteLine("The program have started");
-                ProbarConexion();
-                // MessageBox.Show("The program have started successfully", "Program", MessageBoxButtons.OK);
-                aTimer.BeginInit();
-                aTimer = new System.Timers.Timer(100000);
-                aTimer.Elapsed += OnTimedEvent;
-                aTimer.AutoReset = true;
-                aTimer.Enabled = true;
-                new ManualResetEvent(false).WaitOne();
-                Console.Read();
+                if (Conexion())
+                {
+                    MessageBox.Show("The program have started successfully", "Program");
+
+                    var aTimer = new System.Timers.Timer(10000);
+                    aTimer.Elapsed += OnTimedEvent;
+                    aTimer.AutoReset = true;
+                    aTimer.Enabled = true;
+                    new ManualResetEvent(false).WaitOne();
+                }
+                else
+                {
+                    Console.Beep(2000, 1000);
+                    MessageBox.Show("ERROR: couldn't connect to the dataBase", "Program");
+                }               
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex);
             }
-
         }
 
         private static void OnTimedEvent(Object source, ElapsedEventArgs e)
         {
-            var fechaActual = DateTime.Now.ToString("yyyy-MM-dd h:mm tt");
-            var usgsString = new WebClient().DownloadString("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2017-02-01&minmagnitude=5");
+            var fechaActual = DateTime.Now.ToString("yyyy-MM-dd");
+            var usgsString = new WebClient().DownloadString("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime="+fechaActual+"&minmagnitude=5");
             var model = JsonConvert.DeserializeObject<USGSModel>(usgsString);
-
 
             foreach (USGSModel.Evento evento in model.Feature)
             {
-                DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-                dtDateTime = dtDateTime.AddMilliseconds(evento.Propiedades.Hora).ToUniversalTime();
-                Console.WriteLine(evento.Propiedades.Titulo + " at " + dtDateTime);
-                InsertEvent(evento,dtDateTime);
+                DateTime fechaEvento = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+                fechaEvento = fechaEvento.AddMilliseconds(evento.Propiedades.Hora).ToUniversalTime();
+                InsertEvent(evento.Propiedades, fechaEvento);
             }
-
         }
 
-        public static void InsertEvent(USGSModel.Evento evento, DateTime fecha )
+        public static void InsertEvent(USGSModel.Propiedades propiedades, DateTime fecha )
         {
             try {
-                MySqlCommand comm = con.CreateCommand();
-                comm.CommandText = "INSERT INTO evento(title,code,fecha) VALUES(@title,@code,@fecha)";
-                comm.Parameters.AddWithValue("@title", evento.Propiedades.Titulo);
-                comm.Parameters.AddWithValue("@code", evento.Propiedades.Code);
-                comm.Parameters.AddWithValue("@fecha", fecha);
-                comm.ExecuteNonQuery();
+                if (!SearchExistence(propiedades.Code))
+                {
+                    Console.WriteLine(propiedades.Titulo + " at " + fecha);                    MySqlCommand comm = con.CreateCommand();
+                    comm.CommandText = "INSERT INTO evento(title,code,fecha,magnitud,importancia) VALUES(@title,@code,@fecha,@magnitud,@importancia)";
+                    comm.Parameters.AddWithValue("@title", propiedades.Titulo);
+                    comm.Parameters.AddWithValue("@code", propiedades.Code);
+                    comm.Parameters.AddWithValue("@fecha", fecha);
+                    comm.Parameters.AddWithValue("@magnitud", propiedades.Magnitud);
+                    comm.Parameters.AddWithValue("@importancia", propiedades.Importancia);
+                    comm.ExecuteNonQuery();
+                    Console.Beep(2000, 2000);
+                    MessageBox.Show("There's a new Earthqueake in " + propiedades.Lugar + " Magnitud: " + propiedades.Magnitud, "Program");
+                }
+                else
+                {
+                    Console.WriteLine("The event " + propiedades.Titulo +" Already exists");
+                }                
             }
             catch (Exception e)
             {
-                Console.WriteLine("Evento repetido");
+                Console.WriteLine("Error " + e.Message);
+                Console.Beep(2000, 2000);
             }
         }
 
-
-        public static bool ProbarConexion()
+        private static bool SearchExistence(string code)
         {
-            
-            String servidor = "localhost";
-            String puerto = "3306";
-            String usuario = "root";
-            String password = "admin";
-            String database = "usgs";
+            try {
+                MySqlCommand comm = con.CreateCommand();
+                comm.CommandText = "SELECT code FROM evento WHERE code=@code";
+                comm.Parameters.AddWithValue("@code", code);
+                var query = comm.ExecuteReader();
+                bool exists = query.HasRows;
+                query.Close();
 
-            //Cadena de conexion
-            var cadenaConexion = String.Format("server={0};port={1};user id={2}; password={3}; database={4}", servidor, puerto, usuario, password, database);
-
-            con = new MySqlConnection(cadenaConexion);
-            con.Open();//se abre la conexion
-            if (con.State == System.Data.ConnectionState.Open)
-            {
-                Console.WriteLine("Se conecto con exito a la base de datos");
-                return true;
+                return exists;
             }
-            else
+            catch (Exception e)
             {
-                Console.WriteLine("Error al conectar a la base de datos");
+                Console.WriteLine("Error " + e.Message);
+                return false;
+            }
+        }
+
+        public static bool Conexion()
+        {
+            try
+            {
+                String servidor = "localhost";
+                String puerto = "3306";
+                String usuario = "root";
+                String password = "admin";
+                String database = "usgs";
+
+                //Cadena de conexion
+                var cadenaConexion = String.Format("server={0};port={1};user id={2}; password={3}; database={4}", servidor, puerto, usuario, password, database);
+
+                con = new MySqlConnection(cadenaConexion);
+                con.Open();//se abre la conexion
+                return con.State == System.Data.ConnectionState.Open;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Couldn't connect to BD " + e.Message);
                 return false;
             }
         }
